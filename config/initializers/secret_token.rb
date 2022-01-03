7172f174f8a99f7dfbb55817aa41dc35572fd40e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-03 16:39:04 +0800

    Delete config directory

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Aha::Application.config.secret_token = '06880c0f6d21144c25508af77ffd47a04f89e313e2b9157877bdfe5529bcd71d2653f1f030735527d00fe0f8327a1e06ca3d759d18810bff8766b3e12ad7abb5'
